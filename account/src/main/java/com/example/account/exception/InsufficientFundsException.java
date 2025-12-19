b294d10ec7ca59343468a57c961366d68896ca7e
sahilburade13 <124887043+sahilburade13@users.noreply.github.com>
2025-12-19 21:54:07 +0530

    Delete account directory

--- account/src/main/java/com/example/account/exception/InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.account.exception;
-
-public class InsufficientFundsException extends RuntimeException {
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
